117c48919a5d44692d4ae850f226a3f1144ee532
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-16 03:46:45 +0530

    Day 2

--- src/com/Code_With_Harry/RoughWork.java
+++ src/com/Code_With_Harry/RoughWork.java
@@ -1,7 +1,9 @@
 package com.Code_With_Harry;
 
+import java.util.Scanner;
+
 public class RoughWork {
     public static void main(String[] args) {
-        System.out.println("HEllo ");
+
     }
 }
