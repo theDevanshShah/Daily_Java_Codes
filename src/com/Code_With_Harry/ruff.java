1cc0bbf82f598ff2371c8c795f45d63598d5e46a
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-23 14:49:22 +0530

    String reversal

--- src/com/Code_With_Harry/ruff.java
+++ src/com/Code_With_Harry/ruff.java
@@ -1,14 +1,36 @@
 package com.Code_With_Harry;
 
+import java.io.*;
 import java.util.*;
 
 public class ruff {
-    public static void main(String Args[]) {
 
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        String newX = String.format("%030d", x);
-        System.out.println(newX);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Get the string input from the user
+
+        String input = scanner.nextLine();
+
+        // Convert the string into an array of characters
+        char[] charArray = input.toCharArray();
+
+        // Create a new string to store the reversed characters
+        String reversedString = "";
+
+        // Use a for loop to reverse print the characters
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            reversedString += charArray[i];
+        }
+
+        // Print the reversed string
+        System.out.println(reversedString);
+
+
+        if (input.compareTo(reversedString) == 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
