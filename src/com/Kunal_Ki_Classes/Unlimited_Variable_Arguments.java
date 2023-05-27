e0171c4c594a6649ece23148b9dc2c615cc8a828
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-28 01:02:41 +0530

    Kunal's Classes on Shadowing

---
+++ src/com/Kunal_Ki_Classes/Unlimited_Variable_Arguments.java
@@ -0,0 +1,22 @@
+package com.Kunal_Ki_Classes;
+
+import java.util.Arrays;
+
+public class Unlimited_Variable_Arguments {
+
+    // If I want to take limited arguments for a methods in Java
+    public static void limitedArgs(int A, int B) {
+        System.out.println("A = " + A + " & B = " + B);
+    }
+
+    // But If I want to take UNLIMITED arguments for a methods in Java
+    public static void unLimitedArgs(int... N) {
+        System.out.println("unLimitedArgs has N as " + Arrays.toString(N));
+        // Here Used Arrays.toString() for converting it into ARRAY
+    }
+
+    public static void main(String[] args) {
+        limitedArgs(3, 5);
+        unLimitedArgs(4, 24, 56, 821, 2345, 0);
+    }
+}
