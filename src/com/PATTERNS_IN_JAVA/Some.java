e16e7b4f41bee831d92c10f3e934d13970b49262
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-27 17:38:15 +0530

    Patterns More

--- src/com/PATTERNS_IN_JAVA/Some.java
+++ src/com/PATTERNS_IN_JAVA/Some.java
@@ -2,6 +2,7 @@ package com.PATTERNS_IN_JAVA;
 
 public class Some {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hii");
+        System.out.println("Hii Hello");
+
     }
 }
