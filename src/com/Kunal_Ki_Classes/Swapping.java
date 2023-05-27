067045b3365ca336efc7c88792777537e8fc927b
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-27 21:08:42 +0530

    Kunal's Classes Swapping Function

---
+++ src/com/Kunal_Ki_Classes/Swapping.java
@@ -0,0 +1,34 @@
+package com.Kunal_Ki_Classes;
+
+import java.util.Scanner;
+
+public class Swapping {
+    public static void main(String[] args) {
+        swap();
+    }
+
+    public static void swap() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of A : ");
+        int A = sc.nextInt();
+        System.out.print("Enter the value of A : ");
+        int B = sc.nextInt();
+
+        System.out.println("____________________________");
+        System.out.println("Before Swapping ");
+        System.out.println("Value of A : " + A);
+        System.out.println("Value of B : " + B);
+
+        int Swapper = A;
+        A = B;
+        B = Swapper;
+
+        System.out.println("____________________________");
+
+        System.out.println("After Swapping ");
+        System.out.println("Value of A : " + A);
+        System.out.println("Value of B : " + B);
+
+    }
+}
