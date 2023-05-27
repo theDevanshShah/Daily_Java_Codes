f7ef5caf8212e21bdfd881808ba5adb5a873393d
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-27 21:00:17 +0530

    Kunal's Classes

---
+++ src/com/Kunal_Ki_Classes/Factorial.java
@@ -0,0 +1,29 @@
+package com.Kunal_Ki_Classes;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static double factorial() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        double n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Invalid Number");
+            return 0;
+        }
+
+        double factorial = 1;
+        for (double i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        double factorial = (factorial());
+        System.out.println("Factorial Of Given Number Is : " + factorial);
+    }
+}
+
