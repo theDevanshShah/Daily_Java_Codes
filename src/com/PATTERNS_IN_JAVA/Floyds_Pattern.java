2218fb217253f3b591364ae3b888c0f8c701080a
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-30 21:29:21 +0530

    Floyed's Pattern

--- src/com/PATTERNS_IN_JAVA/Floyds_Pattern.java
+++ src/com/PATTERNS_IN_JAVA/Floyds_Pattern.java
@@ -6,10 +6,14 @@ public class Floyds_Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        int number = 1;
         // Outer Loop
         for (int i = 1; i <= n; i++) {
-
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" " + number + " ");
+                number++;
+            }
+            System.out.println();
         }
 
     }
