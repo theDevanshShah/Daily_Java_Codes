053acff0449bf0d2910607e768740a17a31ea435
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-05-31 01:07:17 +0530

    Binary Triangle Pattern

--- src/com/PATTERNS_IN_JAVA/Binary_Triangle.java
+++ src/com/PATTERNS_IN_JAVA/Binary_Triangle.java
@@ -2,20 +2,25 @@ package com.PATTERNS_IN_JAVA;
 
 import java.util.Scanner;
 
-public class Floyds_Pattern {
+public class Binary_Triangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int number = 1;
+
+
         // Outer Loop
         for (int i = 1; i <= n; i++) {
             // Inner Loop
             for (int j = 1; j <= i; j++) {
-                System.out.print(" " + number + " ");
-                number++;
-            }
-            System.out.println();
-        }
+                int sum = j + i;
+                if (sum % 2 != 0) {
+                    System.out.print(" " + 0 + " ");
+                } else {
+                    System.out.print(" " + 1 + " ");
+                }
 
+            }
+            System.out.println(" ");
+        }
     }
 }
