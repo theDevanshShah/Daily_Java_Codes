c5bccd059c5b71093a011b2984f5359c0e04264d
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-01 23:09:42 +0530

    All Pattern Questions In Java

--- src/com/Code_With_Harry/Practice_Set_3.java
+++ src/com/Code_With_Harry/Practice_Set_3.java
@@ -37,26 +37,34 @@ public class Practice_Set_3 {
 //
 //        System.out.println(" ");
 
-        // Problem 4 : Index of double space and triple space from String
-        System.out.println("Problem 3 : Index of double space and triple space from String ");
-        String string_four = "Hello, This is double space  and this is a tripple space   ";
-        int double_space = string_four.indexOf("  ");
-        System.out.println(double_space);
-        int triple_space = string_four.indexOf("   ");
-        System.out.println(triple_space);
+//        // Problem 4 : Index of double space and triple space from String
+//        System.out.println("Problem 3 : Index of double space and triple space from String ");
+//        String string_four = "Hello, This is double space  and this is a tripple space   ";
+//        int double_space = string_four.indexOf("  ");
+//        System.out.println(double_space);
+//        int triple_space = string_four.indexOf("   ");
+//        System.out.println(triple_space);
 
         System.out.println(" ");
 
         //Problem 5 : Format a string in a Letter Format
-        System.out.println("Problem 5 : Format a string in a Letter Format ");
-        System.out.println(" ");
-        System.out.println("Before Formating");
-        String string_five = "Dear Kashish, I wanted to take a moment to express my heartfelt gratitude for your unwavering support. Your kindness, encouragement, and assistance have meant the world to me, and I am truly thankful for your presence in my life. Your support has made a significant difference, and I am forever grateful.  With sincere appreciation, Devansh Shah";
-        System.out.println(string_five);
-        System.out.println(" ");
-        System.out.println("After Formating");
-        string_five = "Dear Kashish, \n\n I wanted to take a moment to express my heartfelt gratitude for your unwavering support.\n Your kindness, encouragement, and assistance have meant the world to me. \n I am truly thankful for your presence in my life.\n Your support has made a significant difference, and I am forever grateful.  \n\n\t\t\t\t With sincere appreciation, \n\t\t\t\t Devansh Shah";
-        System.out.println(string_five);
+//        System.out.println("Problem 5 : Format a string in a Letter Format ");
+//        System.out.println(" ");
+//        System.out.println("Before Formating");
+//        String string_five = "Dear Kashish, I wanted to take a moment to express my heartfelt gratitude for your unwavering support. Your kindness, encouragement, and assistance have meant the world to me, and I am truly thankful for your presence in my life. Your support has made a significant difference, and I am forever grateful.  With sincere appreciation, Devansh Shah";
+//        System.out.println(string_five);
+//        System.out.println(" ");
+//        System.out.println("After Formating");
+//        string_five = "Dear Kashish, \n\n I wanted to take a moment to express my heartfelt gratitude for your unwavering support.\n Your kindness, encouragement, and assistance have meant the world to me. \n I am truly thankful for your presence in my life.\n Your support has made a significant difference, and I am forever grateful.  \n\n\t\t\t\t With sincere appreciation, \n\t\t\t\t Devansh Shah";
+//        System.out.println(string_five);
 
+
+        System.out.println(" ");
+
+        //Problem 6 : Draw a pattern of triangle
+        System.out.println("Problem 6 : 180 Degree Rotated Left Cornered Triangle");
+        System.out.println(" ");
+        //Outer Loop
+        
     }
 }
