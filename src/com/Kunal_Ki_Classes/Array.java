ef0a4aabdc89e98e14ffe54d02b7ea42253426af
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-03 00:36:01 +0530

    All Pattern Questions In Java

---
+++ src/com/Kunal_Ki_Classes/Array.java
@@ -0,0 +1,18 @@
+package com.Kunal_Ki_Classes;
+
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Array Initialization
+        int[] arrayInt = new int[5];
+        arrayInt[0] = 1;
+        arrayInt[1] = 2;
+        arrayInt[2] = 3;
+        arrayInt[3] = 4;
+        arrayInt[4] = 5;
+        System.out.println(arrayInt[4]);
+    }
+}
