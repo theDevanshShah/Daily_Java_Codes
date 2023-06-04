91f2843ca03431e3b5b4869bbd86dd543236c11e
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-04 13:18:16 +0530

    Arrays

--- src/com/Kunal_Ki_Classes/Array.java
+++ src/com/Kunal_Ki_Classes/Array.java
@@ -40,34 +40,3 @@ public class Array {
 
     }
 }
-//
-//        If you could have a dream romantic date, what would it be like and where would you go? 💭(Be filmy with me I allow it You can tell me anyyyything)
-//
-//        Cuz I knoww you are verryyyy clumsyyy What is the most memorable clumsy moment you've experienced, and how did it turn into a funny or heartwarming memory?
-//        (Ye doraemen jessi dikhne wali kasak kahan ek moment bateyegi yrr !! iski puri life hi funny hogi !!)
-//
-//        What is the most thoughtful gesture someone has ever done for you, and how did it make you feel? (Puch bhi kisse raha hu KASAK & THOUGHTFULNESS ghanta 😂)
-//
-//        When it comes to expressing love and affection, what small gestures mean the most to you?(Dekhte hai bacchi badi ho gai hai yaa abhi bhi nibbi vali harateein hai )
-//
-//        What is the most romantic book, movie, or song that has touched your heart, and why? (Pehle me sirf book puchne wala tha but wo aap mohotarma ke liye out off syllabus ho jayega iss liye mcq diya 😂😶)
-//
-//        If you could have a superpower that helps you understand and empathize with others on a deeper level, what would it be and why?(It's All Fictional, Just wanna know your creativity)
-//
-//        What is one thing you've always wanted to do but have been too afraid or hesitant to try, and what's holding you back? (Kasak & hesitant !! ghanta aadmi ko zinda khaa jaye essi hai meri bestie but answer it honestly open up to me 🖤🤍)
-//
-//        If you could travel anywhere in the world with someone special, where would you go and why? (Just one question is enough for testing your geographic knowledge #parents are not allowed in this trip)
-//
-//        What are your thoughts on love at first sight, and have you ever experienced a moment that made you believe in it?(kasak or pyaar toba toba isse khana dedo bus pitzaa se hi pyaar kar legi yee but still dekhte hai kitni filmy ho sakti hai ye ladki 🎬)
-//
-//        What are the qualities or values that you consider most important in a meaningful and long-lasting friendship or relationship ?
-//
-//        (Long lasting question wo bhi kasak ko !!
-//
-//        nai yrr devansh ye mat kar ye bacchi hai abhi home work baki reh jata hai
-//
-//        iska syllabus khatam nahi ho pata kaahan long-lasting question yrr !!
-//
-//        are dekhte to hai didi ke thoughts kya hai fir future me jab didi ka
-//
-//        break up hoga na tab usse batayenge dekh bachpan me essi thi tu 😂🥰)
