f2c5236648ce7db7a62a5ea215bc0572cc07df79
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-07 03:45:59 +0530

    String Token

---
+++ src/com/First/Funny.java
@@ -0,0 +1,23 @@
+package com.First;
+
+import java.util.Random;
+
+public class Funny {
+
+
+    public static void main(String[] args) {
+        String[] jokes = {
+                "Why don't scientists trust atoms? Because they make up everything!",
+                "Why did the computer go to the doctor? Because it had a virus!",
+                "Why don't skeletons fight each other? They don't have the guts!",
+                "Why couldn't the bicycle find its way home? It lost its bearings!",
+                "Why did the scarecrow win an award? Because he was outstanding in his field!"
+        };
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(jokes.length);
+        String randomJoke = jokes[randomIndex];
+
+        System.out.println("Random Joke: " + randomJoke);
+    }
+}
