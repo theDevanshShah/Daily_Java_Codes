6b81aaaa89a6791581ce824afda39307258dbe9d
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-06 23:04:40 +0530

    String Token

---
+++ src/com/Kunal_Ki_Classes/twoDarray.java
@@ -0,0 +1,14 @@
+package com.Kunal_Ki_Classes;
+
+import java.util.Arrays;
+
+public class twoDarray {
+    public static void main(String[] args) {
+        // This programme is for 2D arrays
+
+        int[][] array2D = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+        System.out.println(array2D.length);
+        System.out.println(Arrays.deepToString(array2D));
+        
+    }
+}
