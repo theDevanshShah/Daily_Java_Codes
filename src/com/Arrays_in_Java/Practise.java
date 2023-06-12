cbeff9856ef01e19d49a89424bbbfc3f0552cdd2
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-13 03:18:00 +0530

    Array Practise Questions

--- src/com/Arrays_in_Java/Practise.java
+++ src/com/Arrays_in_Java/Practise.java
@@ -3,60 +3,42 @@ package com.Arrays_in_Java;
 import java.util.Arrays;
 import java.util.Scanner;
 
-//public class Practise {
-//    public static void main(String[] args) {
-////        float[] array = populating_Array();
-////
-////        float sum = sum(array);
-////        System.out.println(sum);
-//        array_reversal();
-//    }
-//
-//    // Populating array
-//    public static float[] populating_Array() {
-//        System.out.print("Enter the length of array : ");
-//        Scanner sc = new Scanner(System.in);
-//        int lengthArr = sc.nextInt();
-//        float[] array = new float[lengthArr];
-//
-//        for (int i = 0; i < lengthArr; i++) {
-//            System.out.print("Item at index " + i + " : ");
-//            array[i] = sc.nextFloat();
-//        }
-//
-//
-//        System.out.println(Arrays.toString(array));
-//
-//        return array;
-//    }
-//
-//    // Calculating Sum
-//    public static float sum(float[] array) {
-//        float sum = 0.0f;
-//        for (int i = 0; i < array.length; i++) {
-//            sum += array[i];
-//        }
-//
-//        return sum;
-//    }
-
-// Reversing Of Array
 public class Practise {
+
     public static void main(String[] args) {
-        int[] originalArray = {1, 2, 3, 4, 5};
-        int[] reversedArray = reverseArray(originalArray);
-        System.out.println("Original Array: " + Arrays.toString(originalArray));
-        System.out.println("Reversed Array: " + Arrays.toString(reversedArray));
+        // Populating Array
+        int[] mainArray = populatingArray();
+        // Performing Linear Search
+        searching(mainArray);
     }
 
-    public static int[] reverseArray(int[] array) {
-        int[] reversed = new int[array.length];
-        int lastIndex = array.length - 1;
-        for (int i = 0; i < array.length; i++) {
-            reversed[i] = array[lastIndex - i];
+    // Populating Array
+    static int[] populatingArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Length Of An Array : ");
+        int length = sc.nextInt();
+        int[] mainArray = new int[length];
+        for (int i = 0; i < length; i++) {
+            mainArray[i] = sc.nextInt();
         }
-        return reversed;
+        System.out.println(Arrays.toString(mainArray));
+        return mainArray;
+
+    }
+
+    // Performing Linear Search
+    static boolean searching(int[] mainArray) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Target : ");
+        int target = sc.nextInt();
+        for (int i = 0; i < mainArray.length; i++) {
+            if (target == mainArray[i]) {
+                System.out.println("Item at index :" + i);
+                return true;
+            }
+
+        }
+        System.out.println("Item Not Found");
+        return false;
     }
 }
-
-
