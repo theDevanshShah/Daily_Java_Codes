b2e444c7a620a8f8f59c5a79de445e3aec32bc2d
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-12 13:17:18 +0530

    Array Practise Questions

---
+++ src/com/Arrays_in_Java/Practise.java
@@ -0,0 +1,41 @@
+package com.Arrays_in_Java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practise {
+    public static void main(String[] args) {
+        float[] array = populating_Array();
+
+        float sum = sum(array);
+        System.out.println(sum);
+    }
+
+    // Populating array
+    public static float[] populating_Array() {
+        System.out.print("Enter the length of array : ");
+        Scanner sc = new Scanner(System.in);
+        int lengthArr = sc.nextInt();
+        float[] array = new float[lengthArr];
+
+        for (int i = 0; i < lengthArr; i++) {
+            System.out.print("Item at index " + i + " : ");
+            array[i] = sc.nextFloat();
+        }
+
+
+        System.out.println(Arrays.toString(array));
+
+        return array;
+    }
+
+    // Calculating Sum
+    public static float sum(float[] array) {
+        float sum = 0.0f;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+}
