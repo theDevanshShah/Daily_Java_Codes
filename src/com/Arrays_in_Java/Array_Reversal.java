ff511f4a3af08feac1f73a87b22b147df01d4e6d
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-12 23:52:55 +0530

    Array Practise Questions

--- src/com/Arrays_in_Java/Array_Reversal.java
+++ src/com/Arrays_in_Java/Array_Reversal.java
@@ -1,5 +1,54 @@
 //Write a Java program to reverse an array.
 package com.Arrays_in_Java;
 
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
 public class Array_Reversal {
+
+    public static void main(String[] args) {
+        int[] mainArray = populateArray();
+        int[] reversedArray = array_reversal(mainArray);
+        System.out.println(Arrays.toString(reversedArray));
+
+
+    }
+
+    public static int[] populateArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of array : ");
+        int length = sc.nextInt();
+        int[] mainArray = new int[length];
+
+        // Populating Array
+        for (int i = 0; i < length; i++) {
+            System.out.print("At Index no " + i + " the value will be :");
+            mainArray[i] = sc.nextInt();
+        }
+
+        //Accessing The Array
+        System.out.println("\nSo, Your Array Looks Like This\n");
+        System.out.println(Arrays.toString(mainArray));
+
+        return mainArray;
+    }
+
+    public static int[] array_reversal(int[] mainArray) {
+        //Scanner sc = new Scanner(System.in);
+        int[] reversedArray = new int[mainArray.length];
+
+        // MAIN CALCULATION
+        int j = mainArray.length - 1;
+        for (int i = 0; i < mainArray.length; i++) {
+            reversedArray[i] = mainArray[j];
+            j--;
+        }
+
+
+        return reversedArray;
+    }
+
+
 }
--- src/com/Arrays_in_Java/Practise.java
+++ src/com/Arrays_in_Java/Practise.java
@@ -3,39 +3,60 @@ package com.Arrays_in_Java;
 import java.util.Arrays;
 import java.util.Scanner;
 
+//public class Practise {
+//    public static void main(String[] args) {
+////        float[] array = populating_Array();
+////
+////        float sum = sum(array);
+////        System.out.println(sum);
+//        array_reversal();
+//    }
+//
+//    // Populating array
+//    public static float[] populating_Array() {
+//        System.out.print("Enter the length of array : ");
+//        Scanner sc = new Scanner(System.in);
+//        int lengthArr = sc.nextInt();
+//        float[] array = new float[lengthArr];
+//
+//        for (int i = 0; i < lengthArr; i++) {
+//            System.out.print("Item at index " + i + " : ");
+//            array[i] = sc.nextFloat();
+//        }
+//
+//
+//        System.out.println(Arrays.toString(array));
+//
+//        return array;
+//    }
+//
+//    // Calculating Sum
+//    public static float sum(float[] array) {
+//        float sum = 0.0f;
+//        for (int i = 0; i < array.length; i++) {
+//            sum += array[i];
+//        }
+//
+//        return sum;
+//    }
+
+// Reversing Of Array
 public class Practise {
     public static void main(String[] args) {
-        float[] array = populating_Array();
-
-        float sum = sum(array);
-        System.out.println(sum);
+        int[] originalArray = {1, 2, 3, 4, 5};
+        int[] reversedArray = reverseArray(originalArray);
+        System.out.println("Original Array: " + Arrays.toString(originalArray));
+        System.out.println("Reversed Array: " + Arrays.toString(reversedArray));
     }
 
-    // Populating array
-    public static float[] populating_Array() {
-        System.out.print("Enter the length of array : ");
-        Scanner sc = new Scanner(System.in);
-        int lengthArr = sc.nextInt();
-        float[] array = new float[lengthArr];
-
-        for (int i = 0; i < lengthArr; i++) {
-            System.out.print("Item at index " + i + " : ");
-            array[i] = sc.nextFloat();
-        }
-
-
-        System.out.println(Arrays.toString(array));
-
-        return array;
-    }
-
-    // Calculating Sum
-    public static float sum(float[] array) {
-        float sum = 0.0f;
+    public static int[] reverseArray(int[] array) {
+        int[] reversed = new int[array.length];
+        int lastIndex = array.length - 1;
         for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+            reversed[i] = array[lastIndex - i];
         }
-
-        return sum;
+        return reversed;
     }
 }
+
+
