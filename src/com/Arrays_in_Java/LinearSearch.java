89c9e2b575a92ff632c3d86574de82b2e5d6cf50
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-12 13:41:09 +0530

    Array Practise Questions

---
+++ src/com/Arrays_in_Java/LinearSearch.java
@@ -0,0 +1,54 @@
+// Write a program to find out whether a given integer is present in an array or not.
+package com.Arrays_in_Java;
+
+import java.io.OptionalDataException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LinearSearch {
+
+
+    public static void main(String[] args) {
+        int mainArray[] = populateArray();
+        search(mainArray);
+
+
+    }
+
+    public static int[] populateArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of array : ");
+        int length = sc.nextInt();
+        int[] mainArray = new int[length];
+
+        // Populating Array
+        for (int i = 0; i < length; i++) {
+            System.out.print("At Index no " + i + " the value will be :");
+            mainArray[i] = sc.nextInt();
+        }
+
+
+        System.out.println("\nSo, Your Array Looks Like This\n");
+        //Accessing The Array
+        System.out.println(Arrays.toString(mainArray));
+        return mainArray;
+    }
+
+    public static boolean search(int[] mainArray) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Target : ");
+        int target = sc.nextInt();
+
+        for (int i = 0; i < mainArray.length; i++) {
+            if (target == mainArray[i]) {
+                System.out.println("Yes, Target Found At Index No : " + i);
+                return true;
+            }
+        }
+        System.out.println("Target Not Found");
+        return false;
+
+    }
+
+}
+
