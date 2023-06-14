fe1ec978f70d1c50fee7f3bc1183c935b4278e91
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-14 23:20:49 +0530

    Array Practise Questions

---
+++ src/com/Arrays_in_Java/All_in_One.java
@@ -0,0 +1,41 @@
+package com.Arrays_in_Java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class All_in_One {
+
+    // Basic 1D Array
+    // Basic 2D Array
+    // Sum Of Floats
+    // Linear Search
+    // Array Reversal
+    // Average Of Array
+    // Matrix Sum
+
+    public static void main(String[] args) {
+        The_1D_Array_population();
+    }
+
+    public static void The_1D_Array_population() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Length Of Array : ");
+        int length = sc.nextInt();
+        int[] Elements = new int[length];
+        for (int i = 0; i < length; i++) {
+            System.out.print("Enter The Element On Index " + i + " here : ");
+            Elements[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(Elements));
+    }
+
+    public static void The_2D_Array_population() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Rows Of Array : ");
+        int rows = sc.nextInt();
+        System.out.print("Enter The Rows Of Array : ");
+        int cols = sc.nextInt();
+        int[][] Elements = new int[rows][cols];
+
+    }
+}
