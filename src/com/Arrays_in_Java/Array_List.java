1a164f8c004fa3180c14726e6fbbb84dce448abd
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-24 23:23:04 +0530

    Array  List Practise Questions

--- src/com/Arrays_in_Java/Array_List.java
+++ src/com/Arrays_in_Java/Array_List.java
@@ -1,5 +1,6 @@
 package com.Arrays_in_Java;
 
+import java.util.*;
 import java.util.ArrayList;
 
 public class Array_List {
@@ -8,18 +9,15 @@ public class Array_List {
     }
 
     public static void Array_List() {
-        ArrayList<Integer> list = new ArrayList<Integer>(5);
-        ArrayList<Double> double_List = new ArrayList<Double>(5);
-        double_List.add(10.00);
-        double_List.add(10.0);
-        double_List.add(10.1231231);
-        for (int i = 0; i < double_List.size(); i++) {
-            System.out.println(double_List.get(i));
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            list.add(sc.nextInt());
         }
-        System.out.println(double_List.contains(5));
-        System.out.println(double_List.contains(10.00));
-        double_List.set(0, 10.99);
-        System.out.println(double_List.get(0));
-    }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        System.out.println(list);
 
+    }
 }
