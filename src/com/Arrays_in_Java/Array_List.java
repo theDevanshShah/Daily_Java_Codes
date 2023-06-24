e6e57bd593d9ab81e5f77dad279cf07bad025136
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-24 23:25:00 +0530

    Array  List Practise Questions

--- src/com/Arrays_in_Java/Array_List.java
+++ src/com/Arrays_in_Java/Array_List.java
@@ -21,6 +21,7 @@ public class Array_List {
         System.out.println("1st commit");
         System.out.println("2nd commit");
         System.out.println("3rd commit");
+        System.out.println("4th commit");
 
     }
 }
