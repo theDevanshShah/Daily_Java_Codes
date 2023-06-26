951556f8738877d337920ef1d7777d7304565dbb
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-26 23:45:26 +0530

    Spam Commit :(

--- src/com/shutdown/spamm.java
+++ src/com/shutdown/spamm.java
@@ -4,5 +4,6 @@ public class spamm {
     public static void main(String[] args) {
         System.out.println("First Commit");
         System.out.println("Second Commit");
+        System.out.println("Third Commit");
     }
 }
