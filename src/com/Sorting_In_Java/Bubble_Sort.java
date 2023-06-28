54b38529bd6845a31c00bf8295a33d31f4a9b1b4
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-28 22:51:00 +0530

    Bubble Sort

--- src/com/Sorting_In_Java/Bubble_Sort.java
+++ src/com/Sorting_In_Java/Bubble_Sort.java
@@ -7,17 +7,18 @@ public class Bubble_Sort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-        System.out.println(" Enter The Length Of An Array : ");
+        System.out.print("Enter the length of the array: ");
         int N = sc.nextInt();
         int[] array = new int[N];
+
+        System.out.println("Enter the elements of the array:");
         for (int i = 0; i < array.length; i++) {
             array[i] = sc.nextInt();
         }
-        System.out.print("Without Sorting : " + Arrays.toString(array));
+
+        System.out.println("Array before sorting: " + Arrays.toString(array));
 
         // Bubble Sort Logic
-
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
@@ -29,6 +30,6 @@ public class Bubble_Sort {
             }
         }
 
-        System.out.print("\nAfter Sorting: " + Arrays.toString(array));
+        System.out.println("Array after sorting: " + Arrays.toString(array));
     }
 }
