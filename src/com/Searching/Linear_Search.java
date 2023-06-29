9e28eff57e5e7f44934a02cf3b963ae28d5a0022
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-29 14:24:18 +0530

    Modified Linear Search

--- src/com/Searching/Linear_Search.java
+++ src/com/Searching/Linear_Search.java
@@ -1,25 +1,38 @@
 package com.Searching;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Linear_Search {
     public static void main(String[] args) {
-
-        int[] Array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] Array = Populating_Array();
         Linear_Searching(Array);
+    }
+
+    public static int[] Populating_Array() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Length Of The Array : ");
+        int Length = sc.nextInt();
+        int[] Array = new int[Length];
+        for (int i = 0; i < Length; i++) {
+            System.out.print("Enter The Element At Index Number " + (i + 1) + " : ");
+            Array[i] = sc.nextInt();
+        }
+        System.out.println("Your Array Looks Like : " + Arrays.toString(Array));
+        return Array;
     }
 
     public static void Linear_Searching(int[] Array) {
         Scanner sc = new Scanner(System.in);
-        System.out.print(" Enter The Target : ");
+        System.out.print("Enter The Target : ");
         int Target = sc.nextInt();
         for (int i = 0; i < Array.length + 1; i++) {
             if (Target == i) {
-                System.out.println("Yes It IS Found at index number : " + i);
+                System.out.println("Target Found at index number : " + i);
                 return;
             }
         }
-        System.out.println("No");
+        System.out.println("Target Not Found!");
         return;
     }
 }
