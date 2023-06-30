1bf6d052f45b2b03b6483128e58e42f650b3adae
MR. DEVANSH SHAH <87084746+DEVANSHSHAH1947@users.noreply.github.com>
2023-06-30 23:52:54 +0530

    Modified Linear Search

--- src/com/Searching/Linear_Search_In_TwoD_Array.java
+++ src/com/Searching/Linear_Search_In_TwoD_Array.java
@@ -6,17 +6,22 @@ public class Linear_Search_In_TwoD_Array {
                 {1, 2, 3}, {4, 5, 6}, {7, 8, 9}
         };
         int Target = 5;
-        System.out.println(Result(Array, Target));
+        Boolean Final_Result = Result(Array, Target);
+        if (Final_Result = true) {
+            System.out.println("Target Found");
+        } else {
+            System.out.println("Target Not Found");
+        }
     }
 
-    public static int Result(int[][] Array, int Target) {
+    public static boolean Result(int[][] Array, int Target) {
         for (int i = 0; i < Array.length; i++) {
             for (int j = 0; j < Array[i].length; j++) {
                 if (Array[i][j] == Target) {
-                    return Target;
+                    return true;
                 }
             }
         }
-        return 0;
+        return false;
     }
 }
